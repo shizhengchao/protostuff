d883d523c2509a86d3f440cc19ba3b00ae9c6f91
shizhengchao <shizhengchao@outlook.com>
2018-04-03 11:26:57 +0800

    去掉一些无用注释

--- src/main/java/com/szc/blog/UserInfo.java
+++ src/main/java/com/szc/blog/UserInfo.java
@@ -1,17 +1,7 @@
-/**
- * Copyright (c) 2006-2016 Huize Ltd. All Rights Reserved. 
- *  
- * This code is the confidential and proprietary information of   
- * Hzins. You shall not disclose such Confidential Information   
- * and shall use it only in accordance with the terms of the agreements   
- * you entered into with Huize,http://www.huize.com.
- *  
- */
 package com.szc.blog;
 
 /**
- * @author hz17052992
- * @date 2018年4月3日 上午10:20:46
+ * @author shizhengchao
  * @version
  */
 public class UserInfo {
